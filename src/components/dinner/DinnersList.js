b90ca1ae77a44676a0994238dd6a36ebf5891147
user1hunter <user1hunter@gmail.com>
2022-02-04 16:28:21 -0600

    Minimally Viable Product

--- src/components/dinner/DinnersList.js
+++ src/components/dinner/DinnersList.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react"
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+import useSimpleAuth from "../../hooks/ui/useSimpleAuth"
 
 export const DinnersList = () => {
   const [dinners, setDinners] = useState([])
   const history = useHistory()
+  const { getCurrentUser } = useSimpleAuth()
 
   useEffect(() => {
     fetch("http://localhost:8088/dinners?expand_userId")
@@ -31,12 +33,15 @@ export const DinnersList = () => {
     history.push("/create")
   };
 
+  const userId = getCurrentUser().id
+
   return (
     <>
       <h1>Your Dinner Choices</h1>
         <div className="dinners">
           <ul>
-            {dinners.map((dinner) => <div key={`dinner--${dinner.id}`}>{dinner.name}
+            {dinners.filter(dinner => dinner.userId === userId).map((dinner) => <div key={`dinner--${dinner.id}`}>
+            {dinner.name}
               <button key={dinner.id} onClick={() => {deleteDinner(dinner.id)}}>Delete</button>
             </div>)}
           </ul>
--- src/components/generator/Generator.js
+++ src/components/generator/Generator.js
@@ -1,40 +1,36 @@
 import React, { useEffect, useState } from "react"
+import useSimpleAuth from "../../hooks/ui/useSimpleAuth"
 
 export const GenerateDinner = () => {
-  const [dinner, setDinners] = useState([])
-  const [option, setOptions] = useState([])
+  const [dinners, setDinners] = useState([])
+  const [option, setOption] = useState({})
+  const { getCurrentUser } = useSimpleAuth()
 
   useEffect(() => {
-    fetch("http://localhost:8088/dinners?expand_userId")
+    fetch(`http://localhost:8088/dinners?userId=${getCurrentUser().id}`)
       .then((res) => res.json())
       .then((data) => {
         setDinners(data);
       });
   }, [])
-
-  useEffect(() => {
-    fetch("http://localhost:8088/dinners?expand_userId")
-      .then((res) => res.json())
-      .then((data) => {
-        setOptions(data);
-      });
-  }, [])
   
+
   const generateDinner = () => {
-    const len = dinner.length;
-    setOptions(option[Math.floor(Math.random()*len)])
+    const len = dinners.length-1;
+    const randomNumber = Math.floor(Math.random()*len)
+    const test = (dinners[randomNumber])
+    setOption(test)
   }
 
   return (
     <>
       <h1>Generate Your Dinner</h1>
-      <div className="generator--recommendation" key={`dinner--${dinner.id}`}>
+      <div className="generator--recommendation" key={`dinner--${dinners.id}`}>
         Your recommended dinner is: {option.name}
       </div>
       <div className="generator--button">
-        <button key={`button--${dinner.id}`} onClick={() => {generateDinner()}}>Generate Dinner</button>
+        <button onClick={() => {generateDinner()}}>Generate Dinner</button>
       </div>
     </>
   )
-  
 }
